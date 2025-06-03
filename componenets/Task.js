26ebee0d74099f9341b13ec6f6031c02268d933e
hishamedayatt <103420977+hishamedayatt@users.noreply.github.com>
2025-06-03 15:33:44 +0530

    Update Task.js

--- componenets/Task.js
+++ componenets/Task.js
@@ -66,7 +66,7 @@ export default function TaskCard({ task, onDelete, onUpdate }) {
               />
               
               <button
-                type="button"
+                type="submit"
                 className="bg-green-500 text-white px-4 py-2 rounded cursor-pointer hover:scale-105 transition "
               >
                 Save
