800081c9e9fd2d15d3ede344b245e12eccf33ce3
hishamedayatt <103420977+hishamedayatt@users.noreply.github.com>
2025-06-03 10:51:21 +0530

    Update Tabs.js

--- componenets/Tabs.js
+++ componenets/Tabs.js
@@ -4,12 +4,12 @@ const tabNames = ["All", "Pending", "In Progress", "Completed"];
 
 export default function Tabs({ activeTab, onTabChange }) {
   return (
-    <div className="flex gap-6 border-b border-gray-300 mb-4 cursor-pointer">
+    <div className="flex gap-6 border-b border-gray-300 mb-4">
       {tabNames.map((tab) => (
         <button
           key={tab}
           onClick={() => onTabChange(tab)}
-          className={`pb-2 text-lg ${
+          className={`pb-2 text-lg  cursor-pointer ${
             activeTab === tab
               ? "text-black border-b-4 border-green-300 font-semibold"
               : "text-gray-500"
